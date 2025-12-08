bc79e1b7b43b119f122720fb96513709feba4749
nurikdev-hash <nurikdev13@gmail.com>
2025-12-08 20:56:46 +0500

    pages&layout&navbar created, react-router-dom & react-icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './Layout'
+import Login from './pages/login/Login'
+import Catalog from './pages/catalog/Catalog'
+import Cart from './pages/cart/Cart'
+import Product from './pages/product/Product'
+import Profile from './pages/profile/Profile'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route path='' element={<Catalog/>}/>
+            <Route path='product' element={<Product/>}/>
+            <Route path='cart' element={<Cart/>}/>
+            <Route path='profile' element={<Profile/>}/>
+          </Route>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsCart3 } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
+
+function Navbar() {
+    return (
+        <nav className="navbar text-bg-dark border-bottom " data-bs-theme="dark">
+            <div className="container align-items-center">
+                <Link className="navbar-brand" to="/">Catalog</Link>
+                <div className='d-flex'>
+                    <Link to={"/cart"} className='btn btn-outline-success me-3'>
+                        <BsCart3 />
+                    </Link>
+                    <Link to={"/cart"} className='btn btn-outline-primary'>
+                        <BsPerson />
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
